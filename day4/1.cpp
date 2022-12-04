88c0c50e785989cb6f6471e5414b1f509dab7289
franciscopfsimoes <francisco8857@gmail.com>
2022-12-04 11:24:07 +0100

    day 4 is a wrap

---
+++ day4/1.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int is_contained(int assign[2][2]){
+    int res = 0;
+    if(assign[0][0] <= assign[1][0]){
+        if(assign[0][1] >= assign[1][1]){ res = 1; }
+    }
+    if( assign[0][0] >= assign[1][0]){
+        if(assign[0][1] <= assign[1][1]) { res = 1; }
+    }
+    return res;
+}
+
+int main() {
+	ifstream my_file;
+	my_file.open("input.txt", ios::in);
+	if (!my_file) {
+		cout << "No such file";
+        return -1;
+	}
+    string line;
+
+    int sum = 0;
+    string assignments, interval;
+    int assign[2][2]; 
+    while (getline(my_file, line))
+    {
+        stringstream ss(line);
+        for(int j = 0; j < 2; j++){
+            getline(ss, assignments, ',');
+            stringstream sss(assignments);
+            for(int i = 0; i < 2; i++){
+                getline(sss, interval, '-');
+                assign[j][i] = stoi(interval);
+            }
+        }
+        sum += is_contained(assign);
+        //cout << assign[0][0] << " " << assign[0][1] << "\t" << assign[1][0] << " " << assign[1][1] << endl;
+        //cout << is_contained(assign) << endl;
+    }
+	my_file.close();
+    cout << sum << endl;
+	return 0;
+}
